9c43790801c686264774f3820df9f4d06bd8619a
Nicolai <ndi@trueenergy.dk>
2020-02-23 22:07:59 +0100

    Add custom video time and name

--- MiceMonitor_App/app/views.py
+++ MiceMonitor_App/app/views.py
@@ -78,6 +78,7 @@ def convert_videos():
     for video in videos:
         old_path = video
         new_path = video[:-4] + ".mp4"
+        print("%s to %s"%(old_path, new_path))
         os.system("MP4Box -add %s %s"%(old_path, new_path))
 
     return render_template('index.html', record=0)
